96c164c2668930a52724da79a3656de67e688acb
14jsj221 <541238530@qq.com>
2017-08-01 17:51:36 +0800

    The project changed

--- Hello/colormaker.h
+++ Hello/colormaker.h
@@ -1,7 +1,7 @@
 #ifndef COLORMAKER_H
 #define COLORMAKER_H
-
-
+#include <QColor>
+#include <QObject>
 class ColorMaker : public QObject
 {
     Q_OBJECT
--- Hello/main.cpp
+++ Hello/main.cpp
@@ -8,8 +8,10 @@ int main(int argc, char *argv[])
 
     qmlRegisterType<ColorMaker>("an.qt.ColorMaker", 1, 0, "ColorMaker");
 
-    QtQuick2ApplicationViewer viewer;
-    viewer.setMainQmlFile(QStringLiteral("qml/colorMaker/main.qml"));
-    viewer.showExpanded();
+//    QtQuick2ApplicationViewer viewer;
+//    viewer.setMainQmlFile(QStringLiteral("qml/colorMaker/main.qml"));
+//    viewer.showExpanded();
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     return app.exec();
 }
