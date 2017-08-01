3a8ef8e66f424f67d68846ec056079e3fede6a94
14jsj221 <541238530@qq.com>
2017-08-01 15:10:10 +0800

    The project

---
+++ Hello/main.cpp
@@ -0,0 +1,15 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include "colorMaker.h"
+#include <QtQml>
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    qmlRegisterType<ColorMaker>("an.qt.ColorMaker", 1, 0, "ColorMaker");
+
+    QtQuick2ApplicationViewer viewer;
+    viewer.setMainQmlFile(QStringLiteral("qml/colorMaker/main.qml"));
+    viewer.showExpanded();
+    return app.exec();
+}
